cfaf7c1d01cb38a589f939987e1146d71778a051
cristian-robert <38420165+cristian-robert@users.noreply.github.com>
2025-01-06 04:35:45 +0200

    wip

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
+const globalForPrisma = global as { prisma?: PrismaClient };
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: ["query", "error", "warn"],
+  });
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query"],
-  });
+if (!globalForPrisma.prisma) {
+  globalForPrisma.prisma = prismaClientSingleton();
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = globalForPrisma.prisma;
+
+export { prisma };
