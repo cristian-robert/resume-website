871b5a2789359b5832453e62de60739b0d43efaa
cristian-robert <38420165+cristian-robert@users.noreply.github.com>
2025-01-11 05:20:51 +0200

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from "@/components/sections/Hero";
-import About from "@/components/sections/About";
 import Experience from "@/components/sections/Experience";
 import Skills from "@/components/sections/Skills";
 import Contact from "@/components/sections/Contact";
